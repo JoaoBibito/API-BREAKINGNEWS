61787516b9d2b33beaae2ef85010e0d6123bb9b1
JoaoBibito <joao-vitor1903@live.com>
2023-04-05 18:00:45 -0300

    Authorization on create news

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,34 +1,34 @@
-import {createService, findAllService} from "../services/news.service.js";
+import { createService, findAllService } from "../services/news.service.js";
 
-const create = async(req,res)=>{
-  try{
-    const{title,text,banner}=req.body;
-    if(!title || !text || !banner){
-      return res.status(400).send({message:"Submit all fields for registration"})
+const create = async (req, res) => {
+  try {
+    const { title, text, banner } = req.body;
+    if (!title || !text || !banner) {
+      return res.status(400).send({ message: "Submit all fields for registration" })
     }
 
     await createService({
       title,
       text,
       banner,
-      user:"ObjectIdFake"
+      user: req.userId
     })
+  res.send(201)
   }
-  catch(err){
-    return res.status(500).send({message:err.message})
+  catch (err) {
+    return res.status(500).send({ message: err.message })
   }
-res.send(201)
 }
 
-const findAll=async (req,res)=>{
-  const news= await findAllService();
-  if(news.length===0){
-    return res.status(400).send({message:"There are no registered news"})
+const findAll = async (req, res) => {
+  const news = await findAllService();
+  if (news.length === 0) {
+    return res.status(400).send({ message: "There are no registered news" })
   }
   res.send(news)
 }
 
-export  {
+export {
   create,
   findAll
 }
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import userService from "../services/user.service.js";
+
+dotenv.config();
+
+export const authMiddleware = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.status(401);
+    }
+    const parts = authorization.split(" ");
+    const [schema, token] = parts;
+
+    if (parts.length !== 2) {
+      return res.status(401);
+    }
+    if (schema !== "Bearer") {
+      return res.status(401);
+    }
+
+    jwt.verify(token, process.env.SECRET_JWT, async (err, decoded) => {
+      if (err) {
+        return res.status(500).send({ message: "Token invalid" });
+      }
+      const user = await userService.findByIdService(decoded.id);
+      if (!user || !user.id) {
+        return res.status(500).send({ message: "Token invalid" });
+      }
+      req.userId = user.id;
+      return next();
+    })
+  }
+  catch (err) {
+    return res.status(500).send({ message: err })
+  }
+}

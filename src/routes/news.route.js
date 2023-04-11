5dd4a6fdbe4303586d119cb4fd6d1c6eef395605
JoaoBibito <joao-vitor1903@live.com>
2023-04-11 10:27:55 -0300

    create topNews and find by id

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -2,9 +2,12 @@ import {Router} from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-import{create,findAll} from "../controllers/news.controller.js"
+import{create,findAll,topNews,findById} from "../controllers/news.controller.js"
 
 router.post("/",authMiddleware, create);
 router.get("/",findAll);
+router.get("/top",topNews);
+router.get("/:id", findById)
+
 
 export default router
